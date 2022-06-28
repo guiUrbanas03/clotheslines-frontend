f05cc7f768dfe90be4c1f9883b8fc2b9db6d91c8
urbanas <guilherme_urbanas@hotmail.com>
2022-06-28 12:05:24 +0100

    💻 Adding gradient color modes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import ClotheslinesApiService from './services/clotheslinesApiService/clothesLinesApiService';
-import { RootStoreProvider } from './contexts';
+import { ChakraProvider } from '@chakra-ui/react';
 import { Router } from './routes';
+import { theme, themeWithGradient } from './themes/chakra-ui';
+import { observer } from 'mobx-react';
+import { useStores } from './hooks';
+import '@fontsource/poppins/400.css';
+import '@fontsource/poppins/500.css';
+import '@fontsource/poppins/600.css';
+import '@fontsource/poppins/700.css';
 
-const App: React.FunctionComponent = (): JSX.Element | null => {
+const App: React.FunctionComponent = observer((): JSX.Element | null => {
   const [appIsReady, setAppIsReady] = useState<boolean>(false);
+  const { uiStore } = useStores();
 
   useEffect(() => {
     async function prepare() {
       try {
         ClotheslinesApiService.init();
+        uiStore.fetchActiveGradientBackground();
       } catch (error) {
         console.error(error);
       } finally {
@@ -21,10 +30,12 @@ const App: React.FunctionComponent = (): JSX.Element | null => {
   }, []);
 
   return appIsReady ? (
-    <RootStoreProvider>
+    <ChakraProvider
+      theme={uiStore.activeGradientBackground ? themeWithGradient : theme}
+    >
       <Router />
-    </RootStoreProvider>
+    </ChakraProvider>
   ) : null;
-};
+});
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ColorModeScript } from '@chakra-ui/react';
+import { RootStoreProvider } from './contexts';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <RootStoreProvider>
+      <ColorModeScript initialColorMode='light' />
+      <App />
+    </RootStoreProvider>
+  </React.StrictMode>,
 );

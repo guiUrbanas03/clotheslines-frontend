a8a13d32e88d48eb98b01c3ded4e77148cb49a1d
urbanas <guilherme_urbanas@hotmail.com>
2022-06-28 11:54:32 +0100

    💻 Creating MenuDrawer component

---
+++ src/components/MenuDrawer/MenuDrawer.tsx
@@ -0,0 +1,118 @@
+import React, { FunctionComponent } from 'react';
+import {
+  Box,
+  Divider,
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerHeader,
+  HStack,
+  IconButton,
+  Spacer,
+  useColorMode,
+  useColorModeValue,
+  useDisclosure,
+} from '@chakra-ui/react';
+import { FaBars, FaMoon, FaSun, FaTimes } from 'react-icons/fa';
+import { RiHazeFill } from 'react-icons/ri';
+import { GiMoonOrbit } from 'react-icons/gi';
+import { useStores } from '../../hooks';
+import { observer } from 'mobx-react';
+
+const MenuDrawer: FunctionComponent = observer((): JSX.Element => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { setColorMode } = useColorMode();
+  const { uiStore } = useStores();
+
+  const handleChangeColorMode = (
+    mode: 'light' | 'dark',
+    withGradient: boolean,
+  ) => {
+    setColorMode(mode);
+    uiStore.storeActiveGradientBackground(withGradient);
+  };
+
+  return (
+    <>
+      <IconButton
+        opacity={isOpen ? 0 : 1}
+        aria-label='menu'
+        icon={<FaBars />}
+        bgColor={useColorModeValue('#333333', 'dark.ternary')}
+        color='white'
+        onClick={onOpen}
+        transition='opacity 0.5s'
+      />
+      <Drawer isOpen={isOpen} placement='right' onClose={onClose}>
+        <DrawerContent
+          shadow='lg'
+          bgColor={useColorModeValue(
+            'rgba(255, 255, 255, 0.15)',
+            'blackAlpha.100',
+          )}
+          _before={{
+            content: "''",
+            position: 'absolute',
+            width: '100%',
+            height: '100%',
+            background: 'inherit',
+            backdropFilter: 'blur(5px)',
+            zIndex: '-1',
+          }}
+        >
+          <DrawerHeader>
+            <HStack alignItems='center' paddingY={2}>
+              <Box>Menu</Box>
+              <Spacer />
+              <IconButton
+                bgColor='whiteAlpha.100'
+                icon={<FaTimes />}
+                aria-label='close'
+                onClick={onClose}
+              />
+            </HStack>
+            <Divider bgColor='white' />
+          </DrawerHeader>
+          <DrawerBody>
+            <HStack alignItems='stretch' justifyContent='space-between'>
+              <IconButton
+                bgColor={useColorModeValue('whiteAlpha.700', 'dark.ternary')}
+                shadow='md'
+                size='lg'
+                icon={<FaSun />}
+                aria-label='sun'
+                onClick={() => handleChangeColorMode('light', false)}
+              />
+              <IconButton
+                bgColor={useColorModeValue('whiteAlpha.700', 'dark.ternary')}
+                shadow='md'
+                size='lg'
+                icon={<RiHazeFill />}
+                aria-label='moon'
+                onClick={() => handleChangeColorMode('light', true)}
+              />
+              <IconButton
+                bgColor={useColorModeValue('whiteAlpha.700', 'dark.ternary')}
+                shadow='md'
+                size='lg'
+                icon={<FaMoon />}
+                aria-label='moon'
+                onClick={() => handleChangeColorMode('dark', false)}
+              />
+              <IconButton
+                bgColor={useColorModeValue('whiteAlpha.700', 'dark.ternary')}
+                shadow='md'
+                size='lg'
+                icon={<GiMoonOrbit />}
+                aria-label='moon'
+                onClick={() => handleChangeColorMode('dark', true)}
+              />
+            </HStack>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+});
+
+export default MenuDrawer;

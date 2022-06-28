d483aceca57d16bf938eb9a55f6e6642a98e7ce8
urbanas <guilherme_urbanas@hotmail.com>
2022-06-28 11:59:01 +0100

    💻 Applying MainLayout to routes

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
+import { observer } from 'mobx-react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainLayout from '../layouts/main/MainLayout';
 import { HomePage, LoginPage, ProfilePage, RegisterPage } from '../pages';
 
-const Router: React.FunctionComponent = () => {
+const Router: React.FunctionComponent = observer((): JSX.Element => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/'>
-          <Route index element={<HomePage />}></Route>
-          <Route path='register' element={<RegisterPage />}></Route>
-          <Route path='login' element={<LoginPage />}></Route>
-          <Route path='profile' element={<ProfilePage />}></Route>
+        <Route element={<MainLayout />}>
+          <Route path='/'>
+            <Route index element={<HomePage />}></Route>
+            <Route path='register' element={<RegisterPage />}></Route>
+            <Route path='login' element={<LoginPage />}></Route>
+            <Route path='profile' element={<ProfilePage />}></Route>
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
   );
-};
+});
 
 export { Router };

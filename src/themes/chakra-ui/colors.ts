770b85aaa527ee22cf04d4c93de3c0872c97e743
urbanas <guilherme_urbanas@hotmail.com>
2022-06-28 12:02:43 +0100

    🎨 Creating theme colors config

---
+++ src/themes/chakra-ui/colors.ts
@@ -0,0 +1,23 @@
+export default Object.freeze({
+    white: '#FFFFFF',
+    black: '#000000',
+
+    light: {
+        primary: '#D1F3FE',
+        secondary: '#82E0FF',
+        ternary: '#33AFD7',
+        lightGray: '#C4C4C4',
+        mediumGray: '#666666',
+        darkGray: '#333333',
+        gradient: 'linear-gradient(180deg, #BFA2FF 0%, #FED1F1 26.56%, #FEF4D1 100%)'
+    },
+    dark: {
+        primary: '#11254B',
+        secondary: '#233861',
+        ternary: '#202F4B',
+        mediumBlue: '#546EA7',
+        mediumGray: '#666666',
+        darkGray: '#333333',
+        gradient: 'linear-gradient(180deg, #0A1B3D 0%, #193A7A 41.67%, #9E5FA8 80.73%, #D9B49F 100%)'
+    },
+});

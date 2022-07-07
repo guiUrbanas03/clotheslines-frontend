b21f3ad390bdce98fdffaf3ecd08ade39637d6df
urbanas <guilherme_urbanas@hotmail.com>
2022-07-07 22:04:43 +0100

    💻 Creating Profile page

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,8 +1,40 @@
+import { Box, Heading, HStack, IconButton, Text, useColorModeValue } from '@chakra-ui/react';
 import { observer } from 'mobx-react';
 import React from 'react';
+import { FaArrowLeft } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import Card from '../../components/Card/Card';
+import Clothesline from '../../components/Clothesline/Clothesline';
+import UserProfileForm from '../../components/forms/UserProfileForm/UserProfileForm';
 
 export const ProfilePage: React.FunctionComponent = observer(
   (): JSX.Element => {
-    return <div>Profile Page</div>;
+    return (
+      <>
+      <Box ml={['0', '0', '220px']} mt='80px' paddingX={[4, 6, 8]}>
+        <HStack spacing={2}>
+          <Link to='/'>
+            <IconButton
+              icon={<FaArrowLeft />}
+              aria-label='back'
+              bgColor={useColorModeValue('whiteAlpha.600', 'whiteAlpha.200')}
+            />
+          </Link>
+          <Heading size='lg'>Profile</Heading>
+        </HStack>
+      </Box>
+      <Box ml={['0', '0', '183px']}>
+        <Clothesline />
+        <Box paddingX={[4, 6, 8]} mt={2}>
+          <Card
+            withClothespin
+            boxStyle={{ maxWidth: '500px', marginX: 'auto' }}
+          >
+            <UserProfileForm />
+          </Card>
+        </Box>
+      </Box>
+    </>
+    );
   },
 );

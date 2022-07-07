a2b075b377b8419354af553a707663634ea8a68a
urbanas <guilherme_urbanas@hotmail.com>
2022-07-07 22:10:42 +0100

    💻 Fetching user in App component

--- src/App.tsx
+++ src/App.tsx
@@ -9,16 +9,23 @@ import '@fontsource/poppins/400.css';
 import '@fontsource/poppins/500.css';
 import '@fontsource/poppins/600.css';
 import '@fontsource/poppins/700.css';
+import { rootStore } from './stores';
 
 const App: React.FunctionComponent = observer((): JSX.Element | null => {
   const [appIsReady, setAppIsReady] = useState<boolean>(false);
-  const { uiStore } = useStores();
+  const { authStore, uiStore } = useStores();
 
   useEffect(() => {
     async function prepare() {
       try {
         ClotheslinesApiService.init();
+        rootStore.init();
         uiStore.fetchActiveGradientBackground();
+        authStore.fetchIsLoggedIn();
+
+        if (authStore.user == null && authStore.isLoggedIn) {
+          await authStore.fetchUser();
+        }
       } catch (error) {
         console.error(error);
       } finally {

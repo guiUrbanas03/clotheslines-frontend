87cc69754a4985528e1304ff6219e781d1d33cc8
urbanas <guilherme_urbanas@hotmail.com>
2022-07-07 22:06:10 +0100

    💻 Protecting routes

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -3,17 +3,39 @@ import { observer } from 'mobx-react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainLayout from '../layouts/main/MainLayout';
 import { HomePage, LoginPage, ProfilePage, RegisterPage } from '../pages';
+import ProtectedRoute from '../components/ProtectedRoute/ProtectedRoute';
+import { useStores } from '../hooks';
 
 const Router: React.FunctionComponent = observer((): JSX.Element => {
+  const { authStore } = useStores();
+
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<MainLayout />}>
           <Route path='/'>
-            <Route index element={<HomePage />}></Route>
-            <Route path='register' element={<RegisterPage />}></Route>
-            <Route path='login' element={<LoginPage />}></Route>
-            <Route path='profile' element={<ProfilePage />}></Route>
+            <Route
+              element={
+                <ProtectedRoute
+                  hasAccess={!authStore.isAuthenticated}
+                  redirectTo='profile'
+                />
+              }
+            >
+              <Route index element={<HomePage />} />
+              <Route path='login' element={<LoginPage />} />
+              <Route path='register' element={<RegisterPage />} />
+            </Route>
+            <Route
+              element={
+                <ProtectedRoute
+                  hasAccess={authStore.isAuthenticated}
+                  redirectTo='/'
+                />
+              }
+            >
+              <Route path='profile' element={<ProfilePage />} />
+            </Route>
           </Route>
         </Route>
       </Routes>

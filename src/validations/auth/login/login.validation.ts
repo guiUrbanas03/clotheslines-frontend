9e8a6c6470f3c6c643d48e0256e5e9578923529a
urbanas <guilherme_urbanas@hotmail.com>
2022-07-07 22:09:48 +0100

    💻 Updating login validation

--- src/validations/auth/login/login.validation.ts
+++ src/validations/auth/login/login.validation.ts
@@ -1,6 +1,6 @@
 import * as Yup from 'yup';
 
 export const loginValidation = Yup.object({
-  email: Yup.string().email('Valid email').required('Required'),
-  password: Yup.string().required('Required'),
+  email: Yup.string().email('Valid email').max(320, 'Max 320').required('Required'),
+  password: Yup.string().min(8, 'Min 8').max(999, 'Max 999').required('Required'),
 });

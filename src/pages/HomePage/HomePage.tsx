d9bd989d7be42e95e9d4291cf7e417c8f0891ae9
urbanas <guilherme_urbanas@hotmail.com>
2022-07-07 22:03:37 +0100

    💻 Refactoring LoginForm in homepage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,29 +1,11 @@
 import React from 'react';
 import { observer } from 'mobx-react';
-import {
-  Box,
-  Button,
-  Heading,
-  HStack,
-  Input,
-  Text,
-  useColorModeValue,
-  VStack,
-} from '@chakra-ui/react';
+import { Box, Heading, Text } from '@chakra-ui/react';
 import Clothesline from '../../components/Clothesline/Clothesline';
-import { Link } from 'react-router-dom';
 import Card from '../../components/Card/Card';
-import { Formik, FormikProps } from 'formik';
-import FormValidationError from '../../components/FormValidationError/FormValidationError';
-import { loginValidation } from '../../validations/auth/login/login.validation';
-import { LoginFormValue, LoginFormValues } from './HomePage.types';
+import LoginForm from '../../components/forms/LoginForm/LoginForm';
 
 export const HomePage: React.FunctionComponent = observer((): JSX.Element => {
-  const shouldShowError = (
-    formik: FormikProps<LoginFormValues>,
-    field: LoginFormValue,
-  ): boolean => !!(formik.touched[field] && formik.errors[field]);
-
   return (
     <>
       <Box ml={['0', '0', '220px']} mt='80px' paddingX={[4, 6, 8]}>
@@ -39,78 +21,7 @@ export const HomePage: React.FunctionComponent = observer((): JSX.Element => {
             boxStyle={{ maxWidth: '500px', marginX: 'auto' }}
             withClothespin
           >
-            <Formik
-              initialValues={{ email: '', password: '' }}
-              validationSchema={loginValidation}
-              onSubmit={(values) => {
-                console.log(JSON.stringify(values, null, 2));
-              }}
-            >
-              {(formik: FormikProps<LoginFormValues>) => (
-                <form onSubmit={formik.handleSubmit}>
-                  <VStack position='relative' alignItems='stretch' spacing={4}>
-                    <Input
-                      type='email'
-                      size='lg'
-                      placeholder='Email'
-                      fontSize='md'
-                      id='email'
-                      {...formik.getFieldProps('email')}
-                    />
-                    <FormValidationError
-                      show={shouldShowError(formik, 'email')}
-                      message={formik.errors.email}
-                    />
-
-                    <Input
-                      type='password'
-                      size='lg'
-                      fontSize='md'
-                      placeholder='Password'
-                      id='password'
-                      {...formik.getFieldProps('password')}
-                    />
-                    <FormValidationError
-                      show={shouldShowError(formik, 'password')}
-                      message={formik.errors.password}
-                    />
-
-                    <Button
-                      type='submit'
-                      fontWeight='normal'
-                      fontSize='md'
-                      bgColor={useColorModeValue('light.secondary', '#325596')}
-                      size='lg'
-                    >
-                      Login
-                    </Button>
-                    <Text fontSize='md' alignSelf='center'>
-                      or
-                    </Text>
-                    <Button
-                      fontWeight='normal'
-                      bgColor={useColorModeValue('light.primary', '#415F96')}
-                      shadow='md'
-                      size='lg'
-                      fontSize='md'
-                      onClick={() => alert('hello guest!')}
-                    >
-                      Enter as a guest
-                    </Button>
-                    <HStack alignSelf='center'>
-                      <Text fontSize='sm'>New here?</Text>
-                      <Text
-                        fontSize='sm'
-                        color='light.ternary'
-                        textDecoration='underline'
-                      >
-                        <Link to='register'>Register now</Link>
-                      </Text>
-                    </HStack>
-                  </VStack>
-                </form>
-              )}
-            </Formik>
+            <LoginForm />
           </Card>
         </Box>
       </Box>
--- src/pages/HomePage/HomePage.types.ts
+++ src/pages/HomePage/HomePage.types.ts
@@ -2,5 +2,3 @@ export type LoginFormValues = {
   email: string;
   password: string;
 };
-
-export type LoginFormValue = keyof LoginFormValues;

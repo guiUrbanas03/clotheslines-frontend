b4bac0e669c0f394f0e328697db6338b93c6a5aa
urbanas <guilherme_urbanas@hotmail.com>
2022-07-07 22:09:58 +0100

    💻 Updating register validation

--- src/validations/auth/register/register.validation.ts
+++ src/validations/auth/register/register.validation.ts
@@ -1,11 +1,26 @@
 import * as Yup from 'yup';
 
 export const registerUserValidation = Yup.object({
-  email: Yup.string().email('Valid email').required('Required'),
-  nickname: Yup.string().required('Required'),
-  firstName: Yup.string().required('Required'),
-  lastName: Yup.string().required('Required'),
-  country: Yup.string().required('Required'),
-  password: Yup.string().required('Required'),
-  passwordConfirmation: Yup.string().required('Required'),
+  email: Yup.string()
+    .email('Valid email')
+    .max(320, 'Max 320')
+    .required('Required'),
+
+  nickname: Yup.string().max(20, 'Max 20').required('Required'),
+
+  firstName: Yup.string().max(100, 'Max 100').required('Required'),
+
+  lastName: Yup.string().max(100, 'Max 100').required('Required'),
+
+  country: Yup.string().max(320, 'Max 320').required('Required'),
+
+  password: Yup.string()
+    .min(8, 'Min 8')
+    .max(320, 'Max 320')
+    .required('Required'),
+
+  passwordConfirmation: Yup.string()
+    .min(8, 'Min 8')
+    .max(320, 'Max 320')
+    .required('Required'),
 });

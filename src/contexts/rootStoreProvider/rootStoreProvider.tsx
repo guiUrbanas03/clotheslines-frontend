a061ecf9a80985f636c199d5b55afa0d2836be49
urbanas <guilherme_urbanas@hotmail.com>
2022-08-01 03:26:36 +0100

    💻 Adding playlistStore to rootStoreProvider

--- src/contexts/rootStoreProvider/rootStoreProvider.tsx
+++ src/contexts/rootStoreProvider/rootStoreProvider.tsx
@@ -1,5 +1,5 @@
 import { createContext } from 'react';
-import { authStore, userStore, uiStore } from '../../stores';
+import { authStore, userStore, uiStore, playlistStore } from '../../stores';
 import { RootStore } from './rootStoreProvider.types';
 
 export const RootStoreContext = createContext<RootStore>({} as RootStore);
@@ -8,6 +8,7 @@ export const RootStoreProvider = ({ children }: any): JSX.Element => {
   const rootStore: RootStore = Object.freeze({
     authStore: authStore,
     userStore: userStore,
+    playlistStore: playlistStore,
     uiStore: uiStore,
   });
 

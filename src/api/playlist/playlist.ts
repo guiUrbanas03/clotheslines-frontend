288d479a4de2a65963ddc5e6f87859b21db7b8c5
urbanas <guilherme_urbanas@hotmail.com>
2022-08-23 22:30:43 +0100

    💻 Creating hearts (like) system

--- src/api/playlist/playlist.ts
+++ src/api/playlist/playlist.ts
@@ -3,6 +3,7 @@ import {
   playlistsTransformer,
   playlistTransformer,
 } from '../../transformers/playlist/playlistTransformer';
+import { userTransformer } from '../../transformers/user/userTransformer';
 import { PlaylistApi } from './playlist.types';
 
 export const playlist = (axiosApi: AxiosInstance): PlaylistApi => {
@@ -77,6 +78,64 @@ export const playlist = (axiosApi: AxiosInstance): PlaylistApi => {
       console.log(res);
 
       return res;
+    },
+
+    owner: async (playlistId) => {
+      const res = await axiosApi.get(`/playlists/${playlistId}/owner`);
+
+      return {
+        status: res.status,
+        message: res.data.message,
+        data: {
+          user: userTransformer(res.data.user),
+        },
+      };
+    },
+
+    heart: async (playlistId) => {
+      const res = await axiosApi.post(`/playlists/${playlistId}/heart`);
+
+      return {
+        status: res.status,
+        message: res.data.message,
+        data: {
+          heart: res.data.heart,
+        },
+      };
+    },
+
+    unheart: async (playlistId) => {
+      const res = await axiosApi.delete(`/playlists/${playlistId}/heart`);
+
+      return {
+        status: res.status,
+        message: res.data.message,
+        data: {},
+      };
+    },
+
+    getHearts: async () => {
+      const res = await axiosApi.get('/playlists/profile/hearts');
+
+      return {
+        status: res.status,
+        message: res.data.message,
+        data: {
+          hearts: res.data.hearts,
+        },
+      };
+    },
+
+    heartsCount: async (playlistId) => {
+      const res = await axiosApi.get(`/playlists/${playlistId}/hearts/count`);
+
+      return {
+        status: res.status,
+        message: res.data.message,
+        data: {
+          hearts: res.data.hearts,
+        },
+      };
     },
   };
 };
--- src/api/playlist/playlist.types.ts
+++ src/api/playlist/playlist.types.ts
@@ -1,9 +1,14 @@
 import { type Playlist } from '../../models/Playlist/Playlist';
+import { type User } from '../../models/User/User';
 import { ApiResponsePromise as ApiResponse } from '../../types';
 
 type PlaylistId = Playlist['id'];
 
 type PlaylistObject = { playlist: Playlist };
+
+type HeartObject = { heart: any };
+
+type PlaylistHeartsObject = { hearts: Array<Playlist['id']> };
 
 type PaginatedPlaylistsObject = {
   playlists: Array<Playlist>;
@@ -20,11 +25,19 @@ export type PlaylistApi = {
 
   find: (playlistId: PlaylistId) => ApiResponse<PlaylistObject>;
 
-  store: (
-    payload: any,
-  ) => ApiResponse<PlaylistObject>;
+  store: (payload: any) => ApiResponse<PlaylistObject>;
 
   update: (playlistId: PlaylistId, payload: any) => ApiResponse<PlaylistObject>;
 
   destroy: (playlistId: PlaylistId) => Promise<any>;
+
+  owner: (playlistId: PlaylistId) => ApiResponse<{ user: User }>;
+
+  heart: (playlistId: PlaylistId) => ApiResponse<HeartObject>;
+
+  unheart: (playlistId: PlaylistId) => ApiResponse<{}>;
+
+  getHearts: () => ApiResponse<PlaylistHeartsObject>;
+
+  heartsCount: (playlistId: PlaylistId) => ApiResponse<{ hearts: number }>;
 };
